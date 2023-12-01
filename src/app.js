866307ead50713c7fe7a932bf72f8f0ee2aac33c
Emmanuel Guzmán Salas <angelsalasemmanuel@gmail.com>
2023-11-30 18:27:00 -0600

    Funcion Cors
    Hola mundo

--- src/app.js
+++ src/app.js
@@ -3,12 +3,22 @@ import morgan from "morgan";
 import pkg from '../package.json';
 import productroutes from "./routes/productos.routes" 
 import authRoutes from "./routes/auth.routes"
+import { getConnection } from "./database";
+import cors from "cors";
 
 
 const app = express()
 app.set('pkg', pkg)
 app.use(morgan('dev')); 
 app.use(express.json()) //Nos permite recibir los datos en formato json
+
+
+app.use(cors());
+
+app.listen(4000, () => {
+    console.log(`Server listening on port: ${4000}`);
+    getConnection();
+  });
 
 app.get('/',(req, res) => {
     res.json({
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import app from "./app";
 import { getConnection } from "./database";
+import cors from "cors";
+
+// app.use(cors());
+
+// app.listen(4000, () => {
+//     console.log(`Server listening on port: ${4000}`);
+//     getConnection();
+//   });
 
 
-app.listen(4000);
-
-console.log('server listen on port:',4000);
-getConnection();
+// console.log('server listen on port:',4000);
+// getConnection();
 
